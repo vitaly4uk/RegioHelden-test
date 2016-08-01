1a7acb85abd7ab5398ae8d0850a9dca4ca662f41
vitalii omelchuk <vitaly.omelchuk@gmail.com>
2016-08-01 18:31:02 +0300

    make project available to start on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn regiohelden.wsgi --log-file -
--- regiohelden/settings.py
+++ regiohelden/settings.py
@@ -99,6 +99,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
 
 STATIC_URL = '/static/'
 
